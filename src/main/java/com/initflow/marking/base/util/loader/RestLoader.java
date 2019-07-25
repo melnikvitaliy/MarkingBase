0b5e2deb44c2b39f2239e113b2ead2a91ba08b4b
Виталий Мельник <melnik.vitaliy.ur@gmail.com>
2019-07-25 20:36:00 +0300

    -- 1.1-test-6 (add function on RestTemplate )

--- src/main/java/com/initflow/marking/base/util/loader/RestLoader.java
+++ src/main/java/com/initflow/marking/base/util/loader/RestLoader.java
@@ -4,22 +4,31 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import java.nio.charset.Charset;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 
 public class RestLoader {
 
-    private RestLoader(){}
+    private Consumer<RestTemplate> restTemplateInfo;
 
-    public static <T> T get(String url, Map<String, String> headers, Class<T> zlass){
+    public RestLoader(){}
+    public RestLoader(Consumer<RestTemplate> restTemplateInfo) {
+        this.restTemplateInfo = restTemplateInfo;
+    }
+
+    public <T> T get(String url, Map<String, String> headers, Class<T> zlass){
         RestTemplate restTemplate = new RestTemplate();
 
-        restTemplate.getMessageConverters()
-                .add(0, new StringHttpMessageConverter(Charset.forName("UTF-8")));
+        if(restTemplateInfo != null) {
+            restTemplateInfo.accept(restTemplate);
+        }
 
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setAll(headers);
@@ -29,11 +38,12 @@ public class RestLoader {
         return result.getBody();
     }
 
-    public static <T,K> T post(String url, K params, Map<String, String> headers, Class<T> zlass){
+    public <T,K> T post(String url, K params, Map<String, String> headers, Class<T> zlass){
         RestTemplate restTemplate = new RestTemplate();
 
-        restTemplate.getMessageConverters()
-                .add(0, new StringHttpMessageConverter(Charset.forName("UTF-8")));
+        if(restTemplateInfo != null) {
+            restTemplateInfo.accept(restTemplate);
+        }
 
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setAll(headers);
@@ -43,11 +53,12 @@ public class RestLoader {
         return result.getBody();
     }
 
-    public static <K> void put(String url, K params, Map<String, String> headers){
+    public <K> void put(String url, K params, Map<String, String> headers){
         RestTemplate restTemplate = new RestTemplate();
 
-        restTemplate.getMessageConverters()
-                .add(0, new StringHttpMessageConverter(Charset.forName("UTF-8")));
+        if(restTemplateInfo != null) {
+            restTemplateInfo.accept(restTemplate);
+        }
 
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setAll(headers);
@@ -56,7 +67,7 @@ public class RestLoader {
         restTemplate.put(url, entity);
     }
 
-    public static <T,K> T getObjectByRequest(String httpMethod, String url, K params, Map<String, String> headers,
+    public <T,K> T getObjectByRequest(String httpMethod, String url, K params, Map<String, String> headers,
                                              Class<T> zlass){
         headers = headers != null ? headers : new HashMap<>();
         headers.put("Content-Type","application/json;charset=UTF-8");
