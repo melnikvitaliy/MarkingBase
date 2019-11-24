c2b3db64c6c7c1097bc4c449cdf80927452e5f46
Виталий Мельник <melnik.vitaliy.ur@gmail.com>
2019-11-24 19:09:40 +0200

    -- 1.2-SNAPSHOT-6 add headersCont

---
+++ src/main/java/com/initflow/marking/base/constants/ApplicationHeadersConst.java
@@ -0,0 +1,11 @@
+package com.initflow.marking.base.constants;
+
+public class ApplicationHeadersConst {
+
+    public static final String USER_ID_HEADER = "X-USER-ID";
+    public static final String USERNAME_HEADER = "X-USER-USERNAME";
+    public static final String GRANTED_AUTH_HEADER = "X-USER-GRANTED_AUTH";
+    public static final String AUTH_TOKEN_HEADER = "X-Auth-Token";
+
+
+}
