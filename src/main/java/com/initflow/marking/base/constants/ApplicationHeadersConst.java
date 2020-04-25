3f9cd80e30f1e9f44bc42c5a1de0ce11b4bbaff7
Виталий Мельник <melnik.vitaliy.ur@gmail.com>
2020-04-25 22:01:15 +0300

    -- 1.2-SNAPSHOT-22 PRODUCT_GROUP_NAME_HEADER

--- src/main/java/com/initflow/marking/base/constants/ApplicationHeadersConst.java
+++ src/main/java/com/initflow/marking/base/constants/ApplicationHeadersConst.java
@@ -7,6 +7,6 @@ public class ApplicationHeadersConst {
     public static final String GRANTED_AUTH_HEADER = "X-USER-GRANTED_AUTH";
     public static final String AUTH_TOKEN_HEADER = "X-Auth-Token";
     public static final String WAREHOUSE_NAME_HEADER = "x-warehouse-name";
-
+    public static final String PRODUCT_GROUP_NAME_HEADER = "x-user-product-group";
 
 }
