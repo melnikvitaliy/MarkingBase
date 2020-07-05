9d4394cd8992ae6f13b16380e7fa685790fc2f04
Виталий Мельник <melnik.vitaliy.ur@gmail.com>
2020-07-05 23:33:28 +0300

    -- 1.2-SNAPSHOT-23 restLoader add return entity

--- src/main/java/com/initflow/marking/base/util/loader/RestLoader.java
+++ src/main/java/com/initflow/marking/base/util/loader/RestLoader.java
@@ -24,6 +24,10 @@ public class RestLoader {
     }
 
     public <T> T get(String url, Map<String, String> headers, Class<T> zlass){
+        return getEntity(url, headers, zlass).getBody();
+    }
+
+    public <T> ResponseEntity<T> getEntity(String url, Map<String, String> headers, Class<T> zlass){
         RestTemplate restTemplate = new RestTemplate();
 
         if(restTemplateInfo != null) {
@@ -34,11 +38,14 @@ public class RestLoader {
         httpHeaders.setAll(headers);
         HttpEntity entity = new HttpEntity(httpHeaders);
 
-        ResponseEntity<T> result = restTemplate.exchange(url, HttpMethod.GET,  entity, zlass);
-        return result.getBody();
+        return restTemplate.exchange(url, HttpMethod.GET,  entity, zlass);
     }
 
     public <T,K> T post(String url, K params, Map<String, String> headers, Class<T> zlass){
+        return postEntity(url, params, headers, zlass).getBody();
+    }
+
+    public <T,K> ResponseEntity<T> postEntity(String url, K params, Map<String, String> headers, Class<T> zlass){
         RestTemplate restTemplate = new RestTemplate();
 
         if(restTemplateInfo != null) {
@@ -49,8 +56,7 @@ public class RestLoader {
         httpHeaders.setAll(headers);
         HttpEntity<K> entity = new HttpEntity<>(params, httpHeaders);
 
-        ResponseEntity<T> result = restTemplate.postForEntity(url, entity, zlass);
-        return result.getBody();
+        return restTemplate.postForEntity(url, entity, zlass);
     }
 
     public <K> void put(String url, K params, Map<String, String> headers){
@@ -67,6 +73,8 @@ public class RestLoader {
         restTemplate.put(url, entity);
     }
 
+
+
     public <T,K> T getObjectByRequest(String httpMethod, String url, K params, Map<String, String> headers,
                                              Class<T> zlass){
         headers = headers != null ? headers : new HashMap<>();
@@ -76,6 +84,23 @@ public class RestLoader {
                 return get(url, headers, zlass);
             case "POST":
                 return post(url, params, headers, zlass);
+            case "PUT":
+                put(url, params, headers);
+                return null;
+        }
+        return null;
+    }
+
+
+    public <T,K> ResponseEntity<T> getResponseEntityByRequest(String httpMethod, String url, K params, Map<String, String> headers,
+                                      Class<T> zlass){
+        headers = headers != null ? headers : new HashMap<>();
+        headers.put("Content-Type","application/json;charset=UTF-8");
+        switch (httpMethod){
+            case "GET":
+                return getEntity(url, headers, zlass);
+            case "POST":
+                return postEntity(url, params, headers, zlass);
             case "PUT":
                 put(url, params, headers);
                 return null;
