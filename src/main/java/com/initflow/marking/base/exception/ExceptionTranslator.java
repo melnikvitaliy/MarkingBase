768dba2cab4181559f94bfede76df3312e2f7e04
Виталий Мельник <melnik.vitaliy.ur@gmail.com>
2020-08-20 09:56:32 +0300

    -- 1.2-SNAPSHOT-28 return only exception message

--- src/main/java/com/initflow/marking/base/exception/ExceptionTranslator.java
+++ src/main/java/com/initflow/marking/base/exception/ExceptionTranslator.java
@@ -24,49 +24,49 @@ public class ExceptionTranslator {
     @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorVM processMethodNotSupportedException(HttpMessageNotReadableBaseException exception) {
-        return new ErrorVM(HttpStatus.BAD_REQUEST.toString() + "; " + exception.getMessage(), null, ExceptionUtils.getStackTrace(exception));
+        return new ErrorVM(HttpStatus.BAD_REQUEST.toString() + "; " + exception.getMessage(), null, ExceptionUtils.getMessage(exception));
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorVM processMethodNotSupportedException(HttpMessageNotReadableException exception) {
-        return new ErrorVM(HttpStatus.BAD_REQUEST.toString() + "; " + exception.getMessage(), null, ExceptionUtils.getStackTrace(exception));
+        return new ErrorVM(HttpStatus.BAD_REQUEST.toString() + "; " + exception.getMessage(), null, ExceptionUtils.getMessage(exception));
     }
 
     @ExceptionHandler(MetricsBaseRuntimeException.class)
     @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorVM processMethodNotSupportedException(MetricsBaseRuntimeException exception) {
-        return new ErrorVM(HttpStatus.BAD_REQUEST.toString() + "; " + exception.getMessage(), null, ExceptionUtils.getStackTrace(exception));
+        return new ErrorVM(HttpStatus.BAD_REQUEST.toString() + "; " + exception.getMessage(), null, ExceptionUtils.getMessage(exception));
     }
 
     @ExceptionHandler(ForbiddenMetricsBaseRuntimeException.class)
     @ResponseBody
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public ErrorVM processMethodNotSupportedException(ForbiddenMetricsBaseRuntimeException exception) {
-        return new ErrorVM(HttpStatus.FORBIDDEN.toString() + "; " + exception.getMessage(), null, ExceptionUtils.getStackTrace(exception));
+        return new ErrorVM(HttpStatus.FORBIDDEN.toString() + "; " + exception.getMessage(), null, ExceptionUtils.getMessage(exception));
     }
 
     @ExceptionHandler(UnprocessableMetricsBaseRuntimeException.class)
     @ResponseBody
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
     public ErrorVM processMethodNotSupportedException(UnprocessableMetricsBaseRuntimeException exception) {
-        return new ErrorVM(HttpStatus.UNPROCESSABLE_ENTITY.toString() + "; " + exception.getMessage(), null, ExceptionUtils.getStackTrace(exception));
+        return new ErrorVM(HttpStatus.UNPROCESSABLE_ENTITY.toString() + "; " + exception.getMessage(), null, ExceptionUtils.getMessage(exception));
     }
 
     @ExceptionHandler(NotFoundMetricsBaseException.class)
     @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorVM processMethodNotSupportedException(NotFoundMetricsBaseException exception) {
-        return new ErrorVM(HttpStatus.NOT_FOUND.toString() + "; " + exception.getMessage(), null, ExceptionUtils.getStackTrace(exception));
+        return new ErrorVM(HttpStatus.NOT_FOUND.toString() + "; " + exception.getMessage(), null, ExceptionUtils.getMessage(exception));
     }
 
     @ExceptionHandler(NotAllowedMetricsBaseRuntimeException.class)
     @ResponseBody
     @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
     public ErrorVM processMethodNotSupportedException(NotAllowedMetricsBaseRuntimeException exception) {
-        return new ErrorVM(HttpStatus.METHOD_NOT_ALLOWED.toString() + "; " + exception.getMessage(), null, ExceptionUtils.getStackTrace(exception));
+        return new ErrorVM(HttpStatus.METHOD_NOT_ALLOWED.toString() + "; " + exception.getMessage(), null, ExceptionUtils.getMessage(exception));
     }
 
     @ExceptionHandler(Exception.class)
@@ -79,7 +79,7 @@ public class ExceptionTranslator {
             errorVM = new ErrorVM("error." + responseStatus.value().value(), responseStatus.reason(), ex.getMessage());
         } else {
             builder = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR);
-            errorVM = new ErrorVM("error.internalServerError", "Internal server error",  ExceptionUtils.getStackTrace(ex));
+            errorVM = new ErrorVM("error.internalServerError", "Internal server error",  ExceptionUtils.getMessage(ex));
         }
         return builder.body(errorVM);
     }
